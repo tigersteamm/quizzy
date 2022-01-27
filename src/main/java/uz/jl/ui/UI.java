6508cc6c7cdb1395edfac1c398c656c6d4480006
Shahboz <codershaha@gmail.com>
2022-01-26 22:45:39 -0800

    commit

--- src/main/java/uz/jl/ui/UI.java
+++ src/main/java/uz/jl/ui/UI.java
@@ -1,21 +1,30 @@
 package uz.jl.ui;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.bson.types.ObjectId;
 import uz.jl.dto.quiz.QuizCreateDto;
 import uz.jl.dto.user.UserCreateDto;
-import uz.jl.dto.user.UserUpdateDto;
+import uz.jl.entity.quiz.Question;
 import uz.jl.entity.quiz.QuestionMark;
 import uz.jl.entity.quiz.Quiz;
 import uz.jl.entity.quiz.Variant;
+import uz.jl.enums.Language.Language;
+import uz.jl.enums.Level;
+import uz.jl.enums.Subject;
 import uz.jl.exceptions.ApiRuntimeException;
 import uz.jl.response.Data;
 import uz.jl.response.ResponseEntity;
 import uz.jl.security.SecurityHolder;
+import uz.jl.services.question.QuestionService;
 import uz.jl.services.quiz.QuizService;
 import uz.jl.services.users.UserService;
+import uz.jl.ui.question.QuestionUI;
 import uz.jl.utils.Color;
 import uz.jl.utils.Input;
 import uz.jl.utils.Print;
+
+import java.util.ArrayList;
 
 /**
  * @author D4uranbek ср. 20:08. 26.01.2022
@@ -23,13 +32,13 @@ import uz.jl.utils.Print;
 public class UI {
     private final UserService userService;
     private final QuizService quizService;
-//    private final QuestionService questionService;
+    private final QuestionService questionService;
 //    private final VariantService variantService;
 
-    public UI(UserService userService, QuizService quizService) {
+    public UI(UserService userService, QuizService quizService, QuestionService questionService) {
         this.userService = userService;
         this.quizService = quizService;
-//        this.questionService = questionService;
+        this.questionService = questionService;
 //        this.variantService = variantService;
     }
 
@@ -37,76 +46,72 @@ public class UI {
     /**
      * auth ui
      */
-    //------------------------------------------AuthUI----------------------------------------------------------------------------------------
-
-    public void register() {
-        UserCreateDto dto = UserCreateDto.childBuilder().username(Input.getStr("Username: ")).password(Input.getStr("Password:")).language(Input.getStr("Language:")).build();
-        ResponseEntity<Data<ObjectId>> response = userService.create(dto);
-        Print.println(response.getData().getBody());
+    public void login() {
+        try {
+            String username = Input.getStr("username ");
+            String password = Input.getStr("password ");
+            userService.login(username, password);
+        } catch (ApiRuntimeException e) {
+            showResponse(e.getMessage());
+        }
     }
 
-    public void login() {
-        String username = Input.getStr("Username: ");
-        String password = Input.getStr("Password: ");
-        ResponseEntity<Data<Boolean>> response = userService.login(username, password);
-        Print.println(SecurityHolder.session.getUsername() + " " + response.getData().getBody());
+    public void register() {
+        UserCreateDto dto = UserCreateDto.childBuilder()
+                .username("Student")
+                .password("Student")
+                .language("RU")
+                .role("STUDENT")
+                .build();
+        userService.create(dto);
     }
 
     public void logout() {
-        SecurityHolder.killSession();
+        userService.logout();
+        Print.println(Color.BLUE, "Bye...");
     }
 
     public void userCreate() {
         try {
-            String username = Input.getStr("username: ");
-            String fullName=Input.getStr("FullName: ");
-            String password = Input.getStr("password: ");
-            String language = Input.getStr("language: ");
-            UserCreateDto dto=new UserCreateDto(username,fullName,password,language);
-            ResponseEntity<Data<ObjectId>> dataResponseEntity = userService.create(dto);
-            showResponse(dataResponseEntity.getData().getBody());
+            String username = Input.getStr("username ");
+            String password = Input.getStr("password ");
+            userService.login(username, password);
         } catch (ApiRuntimeException e) {
             showResponse(e.getMessage());
-        }
-    }
-    public void userUpdate() {
-        try {
-            String id=Input.getStr("UserId");
-            String username = Input.getStr("username: ");
-            String fullName=Input.getStr("FullName: ");
-            String language = Input.getStr("language: ");
-            UserUpdateDto dto=new UserUpdateDto(id,username,fullName,language);
-            ResponseEntity<Data<Void>> update = userService.update(dto);
-            showResponse(update.getData().getBody());
-        } catch (ApiRuntimeException e) {
-            showResponse(e.getMessage());
-        }
-    }
-    public void userDelete(){
-        try{
-            String id=Input.getStr("UserId");
-               userService.delete(new ObjectId(id));
-        }catch (ApiRuntimeException e){
-            showResponse (e.getMessage());
         }
     }
 
     public void loginAsStudent() {
-        String username = "hello";
-        String password = "@helloHello007_";
+        String username = "Student";
+        String password = "Student";
         userService.login(username, password);
     }
 
 
-
     public void solveQuiz() {
         try {
-            String language = Input.getStr("language ");
+            String language = Input.getStr("language: ");
             String subject = Input.getStr("subject ");
             String level = Input.getStr("level ");
             String count = Input.getStr("count ");
 
+            Quiz quiz = Quiz.childBuilder()
+                    .language(Language.valueOf(language))
+                    .subject(Subject.valueOf(subject))
+                    .level(Level.valueOf(level))
+                    .questionsMarks(new ArrayList<>())
+                    .build();
+            for (int i = 0; i < Integer.parseInt(count); i++) {
+                Question random = questionService.getRandom(language, subject, level);
+                QuestionMark questionMark = QuestionMark.childBuilder()
+                        .question(random).build();
 
+                quiz.getQuestionsMarks().add(questionMark);
+            }
+
+//            SecurityHolder.session.setCurrentQuiz(quiz);
+
+            System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(quiz));
 
 
 //            QuizCreateDto dto = QuizCreateDto.childBuilder()
